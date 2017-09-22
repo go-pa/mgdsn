7a49da64d33d26626e034e950a1d56bf53fd9580
Thomas Frössman <thomasf@jossystem.se>
2017-09-22 21:58:40 +0200

    Add flag test

--- dsn_test.go
+++ dsn_test.go
@@ -1,6 +1,7 @@
 package mgdsn
 
 import "testing"
+import "flag"
 
 func TestDSN(t *testing.T) {
 	type testCase struct {
@@ -20,4 +21,24 @@ func TestDSN(t *testing.T) {
 			t.Fatal("should have failed", v.in, d.String())
 		}
 	}
+}
+
+func TestFlag(t *testing.T) {
+	fs := flag.NewFlagSet("test", flag.ContinueOnError)
+	var dsn DSN
+	fs.Var(&dsn, "testdsn", "testtest")
+	err := fs.Parse([]string{"-testdsn", "domain=1 api_key=2 public_api_key=3"})
+	if err != nil {
+		t.Fatal(err)
+	}
+	if dsn.Domain != "1" {
+		t.Fatal("domain", dsn.Domain)
+	}
+	if dsn.APIKey != "2" {
+		t.Fatal("api key", dsn.APIKey)
+	}
+	if dsn.PublicAPIKey != "3" {
+		t.Fatal("pub pi key", dsn.PublicAPIKey)
+	}
+
 }
